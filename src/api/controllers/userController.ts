31cbdaf6ac3859f66d480e58d1c168adeabb7204
Youssef Dawoud <41302314+DeathPancake1@users.noreply.github.com>
2022-03-21 13:53:36 +0200

    created user Controller

---
+++ src/api/controllers/userController.ts
@@ -0,0 +1,29 @@
+import { Router, Response, Request } from 'express';
+import UsersStore from '../models/users';
+import { User, UserAuth } from '../interfaces/user';
+import { authToken } from '../../authenticator';
+
+export const UserController: Router = Router()
+const user: UsersStore = new UsersStore()
+
+UserController.get('/', async (req : Request, res : Response) => {
+    const users: User[] = await user.index()
+    return res.json(users)
+})
+
+UserController.get('/:id', async (req : Request, res : Response) => {
+    const userID = parseInt(req.params.id)
+    const retUser: User = await user.getUser(userID)
+    return res.json(retUser)
+})
+
+UserController.post('/', authToken, async (req : Request, res : Response) => {
+    const createdUser: UserAuth = await user.createUser(req.body)
+    return res.json(createdUser)
+})
+
+UserController.delete('/:id', authToken, async (req : Request, res : Response) => {
+    const userID: number = parseInt(req.params.id)
+    const deletedUser: User = await user.deleteUser(userID)
+    return res.json(deletedUser)
+})
