2100cfbee9b505aba8a04d39f37b38dcb4edea2d
Youssef Dawoud <41302314+DeathPancake1@users.noreply.github.com>
2022-03-21 12:00:31 +0200

    made order controller

--- src/api/controllers/OrderController.ts
+++ src/api/controllers/OrderController.ts
@@ -2,12 +2,60 @@ import { Router, Response, Request } from 'express';
 
 import OrdersStore from '../models/orders';
 import { Order } from '../interfaces/order';
-import { generateToken } from '../../authenticator'
+import { authToken } from '../../authenticator';
 
 export const OrderController: Router = Router()
 const order: OrdersStore = new OrdersStore()
 
 OrderController.get(
-    '/:user_id',
-    authToken
+    '/all',
+    authToken ,
+   async (req:Request, res: Response) => {
+    const orders: Order[] = await order.index();
+    return res.json(orders);
+   }
 )
+
+OrderController.get(
+    '/current/:user_id',
+    authToken ,
+   async (req:Request, res: Response) => {
+    const userId: number = parseInt(req.params.user_id);
+    const currentOrder: Order = await order.getOrderByUserID(userId);
+    return res.json(currentOrder);
+   }
+)
+
+OrderController.put(
+    '/', 
+    authToken, 
+    async (req: Request, res: Response) => {
+        const status = req.query.status as string;
+        const orderId = parseInt(req.query.orderId as string);
+    
+        if (orderId && ['active', 'complete'].includes(status)) {
+            const updatedOrder: Order = await order.updateOrder(
+                orderId,
+                status
+            );
+            return res.json(updatedOrder);
+        } else {
+            return res.status(400).json({ Error: 'Bad parameters' });
+        }
+  });
+
+OrderController.put(
+    '/:id', 
+    authToken, 
+    async (req: Request, res: Response) => {
+        const orderId = parseInt(req.query.orderId as string);
+    
+        if (orderId) {
+            const deletedOrder: Order = await order.deleteOrder(
+                orderId
+            );
+            return res.json(deletedOrder);
+        } else {
+            return res.status(400).json({ Error: 'Bad parameters' });
+        }
+  });
